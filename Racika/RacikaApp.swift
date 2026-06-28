1145610b58380b679f03305c8973109724774988
kouseina <kouseinadaffa@gmail.com>
2026-06-28 10:49:43 +0700

    Initial Commit

---
+++ Racika/RacikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RacikaApp.swift
+//  Racika
+//
+//  Created by Daffa Putera Kouseina on 28/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RacikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
